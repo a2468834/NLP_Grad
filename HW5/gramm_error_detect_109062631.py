f884a5597518a6d9f64d3ebd1b89af9cc2c33fc6
a2468834 <a2468834@gmail.com>
2020-11-06 20:03:49 +0800

    Change HW5 from Internet version to local data set version.

--- HW5/gramm_error_detect_109062631.py
+++ HW5/gramm_error_detect_109062631.py
@@ -10,60 +10,91 @@
 #   Environment:    
 #      Software:    Python 3.8.5 on 64-bit Windows 10 Pro v2004
 #      Hardware:    Intel i7-10510U, 16GB DDR4 non-ECC ram, and no discrete GPU
+import ast
 from   linggle import Linggle
+from   multiprocessing import Pool
 import os
 import pathlib
+import pickle
 import re
 import requests
+import sys
+import zlib
+
+# The abbreviation "IW" means "incorrect word".
 
 class FUNC:
     tokenize = lambda text : re.findall(r'\w+|[,.?]', text.lower())
+    
+    def zlib_comp(uncompression): # Use highest level of compression
+        return zlib.compress(pickle.dumps(uncompression), level=9)
+    
+    def zlib_decomp(compression):
+        return pickle.loads(zlib.decompress(compression))
 
 
 class CONST:
-    file_name = lambda : 'sentences-test.tsv'
-    #file_name = lambda : 'sentences.tsv'
-    OKGREEN   = lambda : '\033[92m'
-    ENDC      = lambda : '\033[0m'
+    file_name    = lambda : 'sentences-test.tsv'
+    DB_file_name = lambda : "sb.dist.tsv"
+    #file_name   = lambda : 'sentences.tsv'
+    OKGREEN      = lambda : '\033[92m'
+    ENDC         = lambda : '\033[0m'
     
 
-
-def get_sbg_stat(sbg):
-    '''
+def get_sbg_stat(mode, sbg, sbg_DB=None):
+    ''' Usage of mode == 'NETWORK'
     1.  Example usage of get_sbg_stat()
         > $ get_sbg_stat('affected our')
         > {'1': 45440, '2': 29769, '3': 3005, '4': 4111}
     2.  Query example url
         http://thor.nlplab.cc:5566/search?q={skip_bigram}
     '''
-    response = requests.get('http://thor.nlplab.cc:5566/search?q=' + sbg.lower())
-    return response.json()
+    if mode == 'NETWORK':
+        response = requests.get('http://thor.nlplab.cc:5566/search?q=' + sbg.lower())
+        response = response.json()
+    elif mode == 'LOCAL':
+        try:
+            response = FUNC.zlib_decomp(sbg_DB[sbg.lower()])
+        except:
+            print("Error when accessing sbg_DB.")
+            exit()
+        response = {str(tuple0): tuple1 for tuple0, tuple1 in response}
+    else:
+        print("Error mode of get_sbg_stat().")
+        exit()
+    
+    return response
 
 
-# Return a list of lists: [[sentence_0], [sentence_1],...], where sentence_0 = [(word_0, 'c/i'), (word_1, 'c/i'),...]
+# Return a list of dicts: [{'token' : token_sentence, 'IW_index' : IW_index),...], 
+# where token_sentence = ['word_0', 'word_1',...]
 def readTSV():
     working_dir   = pathlib.Path().absolute()
     tsv_file_path = os.path.join(working_dir, CONST.file_name())
+        
+    with open(tsv_file_path, 'r') as fp: whole_file = fp.readlines()
     
-    result = []
-    
-    with open(tsv_file_path, 'r') as fp:
-        whole_file = fp.readlines()
-    
-    one_sent = []
-    for index in range(len(whole_file)):
-        if (whole_file[index] == "\n"):
-            result.append(one_sent)
-            one_sent = []
-        elif (index == len(whole_file)-1):
-            # Append the last line of input file
-            temp = FUNC.tokenize(whole_file[index])
-            one_sent.append((temp[0], temp[1]))
-            result.append(one_sent)
-            one_sent = []
+    one_sent, result = [], []
+    IW_index, prev_sent_index = 0, 0
+    for itr in range(len(whole_file)):
+        if (whole_file[itr] == "\n"):
+            # Append a complete info. of a single sentence into return list
+            result.append({'token': one_sent, 'IW_index': IW_index - prev_sent_index})
+            one_sent, IW_index = [], 0
+            prev_sent_index = itr + 1
+        elif (itr == len(whole_file)-1):
+            temp = FUNC.tokenize(whole_file[itr])
+            one_sent.append(temp[0])
+            IW_index = itr if (temp[1] == 'i') else IW_index
+            
+            # Append a complete info. of a single sentence into return list
+            result.append({'token': one_sent, 'IW_index': IW_index - prev_sent_index})
+            one_sent, IW_index = [], 0
+            prev_sent_index = itr + 1
         else:
-            temp = FUNC.tokenize(whole_file[index])
-            one_sent.append((temp[0], temp[1]))
+            temp = FUNC.tokenize(whole_file[itr])
+            one_sent.append(temp[0])
+            IW_index = itr if (temp[1] == 'i') else IW_index
     
     return result
 
@@ -72,26 +103,21 @@ def readTSV():
 # del_cands = [('affected our', skipgram distance of prev. string)]
 # ins_cands = [('to our', skipgram dist.), ('to life', skipram dist.)]
 def makeCandidates(query_sent):
-    del_cands, ins_cands, incorrect_word = [], [], ""
+    del_cands, ins_cands = [], []
+    IW_index = query_sent['IW_index']
     
-    for index in range(len(query_sent)):
-        if query_sent[index][1] == 'c':
-            pass
-        else: #query_sent[index][1] == 'i'
-            incorrect_word = query_sent[index][0]
+    # Deletion candidates
+    del_cands.append((query_sent['token'][IW_index-1] + ' ' + query_sent['token'][IW_index+1], 2))
+    
+    # Insertion candidates
+    ins_cands.append((query_sent['token'][IW_index] + ' ' + query_sent['token'][IW_index+1], 1))
+    ins_cands.append((query_sent['token'][IW_index] + ' ' + query_sent['token'][IW_index+2], 2))
             
-            # Deletion candidates
-            del_cands.append((query_sent[index-1][0] + ' ' + query_sent[index+1][0], 2))
-            
-            # Insertion candidates
-            ins_cands.append((query_sent[index][0] + ' ' + query_sent[index+1][0], 1))
-            ins_cands.append((query_sent[index][0] + ' ' + query_sent[index+2][0], 2))
-            
-    return del_cands, ins_cands, incorrect_word
+    return del_cands, ins_cands
 
 
-def calcDistPercent(cand_str):
-    dist_dict = get_sbg_stat(cand_str)
+def calcDistPercent(cand_str, sbg_DB=None):
+    dist_dict = get_sbg_stat('LOCAL', cand_str, sbg_DB=sbg_DB)
     
     total_num = sum(dist_dict.values())
     dist_dict = { key : round(dist_dict[key] / total_num * 100.0, 4) for key in dist_dict}
@@ -112,23 +138,17 @@ def list2StrSent(sent_list):
     return output_sentence[0:-1]
 
 
-def printResult(print_sent_list, incorrect_word, mode):
-    print_sent_list = [itr[0] for itr in print_sent_list]
-    
-    try:
-        index = print_sent_list.index(incorrect_word)
-    except:
-        print("Cannot find incorrect word in the query sentence.")
-        exit()
+def printResult(query_sent, mode, IW_index=None):
+    print_sent_list = query_sent['token'].copy() # Make sure the assignment id a pass-by-value copy.
     
     if mode == "DEL":
-        print_sent_list[index] = '?' + print_sent_list[index]
+        print_sent_list[IW_index] = '?' + print_sent_list[IW_index]
         print(f"{CONST.OKGREEN()}%s{CONST.ENDC()}"%list2StrSent(print_sent_list))
     elif mode == "INS0":
-        print_sent_list[index] = '_ ' + print_sent_list[index]
+        print_sent_list[IW_index] = '_ ' + print_sent_list[IW_index]
         print(f"{CONST.OKGREEN()}%s{CONST.ENDC()}"%list2StrSent(print_sent_list))
     elif mode == "INS1":
-        print_sent_list[index] = print_sent_list[index] + ' _'
+        print_sent_list[IW_index] = print_sent_list[IW_index] + ' _'
         print(f"{CONST.OKGREEN()}%s{CONST.ENDC()}"%list2StrSent(print_sent_list))
     elif mode == "ORIGIN":
         print("Sentence: %s"%list2StrSent(print_sent_list))
@@ -137,28 +157,62 @@ def printResult(print_sent_list, incorrect_word, mode):
         exit()
 
 
+# Read data using multiprocessing Pool
+def readDataTSVParallel():
+    pool = Pool()
+    
+    working_dir   = pathlib.Path().absolute()
+    tsv_file_path = os.path.join(working_dir, CONST.DB_file_name())
+    
+    print("Open data tsv file.")
+    with open(tsv_file_path) as fp:
+        input_data = fp.read().splitlines()
+    
+    print("Start parallel computing.")
+    return {key_value[0]: key_value[1] for key_value in pool.map(mapper, input_data)}
+
+
+def mapper(each_line):
+    temp  = re.split(r'\t+', each_line)
+    key   = temp[0]
+    value = list(ast.literal_eval(temp[1]))
+    value = FUNC.zlib_comp(value)
+    return (key, value)
+
+
 # Main function
 if __name__ == "__main__":
     query_sent_list = readTSV()
+    sbg_DB = readDataTSVParallel()
     
     for query_sent in query_sent_list:
-        del_cands, ins_cands, incorrect_word = makeCandidates(query_sent)
+        del_cands, ins_cands = makeCandidates(query_sent)
         
-        del_dist_dict_0 = calcDistPercent(del_cands[0][0])
-        ins_dist_dict_0 = calcDistPercent(ins_cands[0][0])
-        ins_dist_dict_1 = calcDistPercent(ins_cands[1][0])
+        del_dist_dict_0 = calcDistPercent(del_cands[0][0], sbg_DB=sbg_DB)
+        ins_dist_dict_0 = calcDistPercent(ins_cands[0][0], sbg_DB=sbg_DB)
+        ins_dist_dict_1 = calcDistPercent(ins_cands[1][0], sbg_DB=sbg_DB)
         
         del_score = del_dist_dict_0['1']
         ins_score = (ins_dist_dict_0['2'] + ins_dist_dict_1['3']) / 2.0
         
         if del_score > ins_score:
-            printResult(query_sent, incorrect_word, "ORIGIN")
+            printResult(query_sent, "ORIGIN", query_sent['IW_index'])
             print(f"{CONST.OKGREEN()}Answer: Delete{CONST.ENDC()}")
-            printResult(query_sent, incorrect_word, "DEL")
+            printResult(query_sent, "DEL", query_sent['IW_index'])
         else:
-            printResult(query_sent, incorrect_word, "ORIGIN")
+            printResult(query_sent, "ORIGIN", query_sent['IW_index'])
             print(f"{CONST.OKGREEN()}Answer: Insert{CONST.ENDC()}")
-            printResult(query_sent, incorrect_word, "INS0")
-            printResult(query_sent, incorrect_word, "INS1")
+            printResult(query_sent, "INS0", query_sent['IW_index'])
+            printResult(query_sent, "INS1", query_sent['IW_index'])
         print("\n\n")
- 
+
+'''
+linggle = Linggle()
+
+linggle.query('am replying your _ letter')
+# []
+
+linggle.query('am replying _ your letter')
+# [['am replying to your letter', 245]]
+
+'''
